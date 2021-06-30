1483c47f65532a8ee49efe8c756b58fee954ee66
chris <chrissx@chrissx.de>
2021-06-30 21:13:14 +0200

    i actually managed to get it working

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/chrissxMedia/fonts.chrissx.de
+
+go 1.16
---
+++ hanna.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+var fonts = map[string]struct {
+	name   string
+	online [][2]string
+}{
+	"/impact": {name: "Impact", online: [][2]string{
+		{"https://db.onlinewebfonts.com/t/6330ddc0d8e61db73c521dbe6288743b.eot?#iefix", "embedded-opentype"},
+		{"https://db.onlinewebfonts.com/t/6330ddc0d8e61db73c521dbe6288743b.woff2", "woff2"},
+		{"https://db.onlinewebfonts.com/t/6330ddc0d8e61db73c521dbe6288743b.ttf", "truetype"},
+	}},
+	"/inter": {name: "Inter", online: [][2]string{
+		{"https://fonts.gstatic.com/s/inter/v3/UcCO3FwrK3iLTeHuS_fvQtMwCp50KnMw2boKoduKmMEVuLyfAZ9hjp-Ek-_EeA.woff", "woff"},
+	}},
+	"/ubuntu": {name: "Ubuntu", online: [][2]string{
+		{"https://fonts.gstatic.com/s/ubuntu/v15/4iCs6KVjbNBYlgoKfw72nU6AFw.woff2", "woff2"},
+	}},
+	"/minecraft": {name: "Minecraft", online: [][2]string{
+		{"https://db.onlinewebfonts.com/t/6ab539c6fc2b21ff0b149b3d06d7f97c.woff", "woff"},
+	}},
+	"/woodcut": {name: "Woodcut", online: [][2]string{
+		{"https://zerm.eu/Woodcut.ttf", "truetype"},
+	}},
+}
+
+func getCss(url string) string {
+	font := fonts[url]
+	css := "@font-face {"
+	css += "font-family: '" + font.name + "'; "
+	css += "src: local('" + font.name + "')"
+	for _, file := range font.online {
+		css += ", url(" + file[0] + ") format('" + file[1] + "')"
+	}
+	css += "; }"
+	return css
+}
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("Got a %s request from %s: %s (%s)",
+			r.Proto, r.RemoteAddr, r.URL, r.Host)
+		if r.URL.Path == "/" {
+			w.Header().Add("content-type", "text/css")
+			for k := range fonts {
+				fmt.Fprintln(w, getCss(k))
+			}
+		} else if _, ok := fonts[r.URL.Path]; ok {
+			w.Header().Add("content-type", "text/css")
+			fmt.Fprint(w, getCss(r.URL.Path))
+		} else {
+			w.WriteHeader(404)
+			fmt.Fprint(w, "Font \""+r.URL.Path+"\" not found.")
+		}
+	})
+
+	http.ListenAndServe(":8042", nil)
+}
